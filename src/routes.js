feb57310914ea9c0eba3cc051a7c336cb0a83ada
Serhii Ka <serhiika.dev@gmail.com>
2022-02-05 01:37:52 +0200

    fix create route

--- src/routes.js
+++ src/routes.js
@@ -37,7 +37,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
